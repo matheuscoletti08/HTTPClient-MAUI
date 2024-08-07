cfd5bead61c25673a81ab3d52f52d730eda74e85
Coletti <matheus.coletti08@gmail.com>
2024-08-07 17:25:58 -0300

    feat: create Post class

--- Model/Post.cs
+++ Model/Post.cs
@@ -9,5 +9,8 @@ namespace HTTPClient.Model
     public class Post
     {
         public string userId { get; set; }
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
     }
 }
