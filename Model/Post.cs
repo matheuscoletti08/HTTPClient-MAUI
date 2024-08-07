94d6eeacb6fe00ab9fb93b34aea76b20c6512057
Coletti <matheus.coletti08@gmail.com>
2024-08-07 17:21:46 -0300

    feat: initial commit

---
+++ Model/Post.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HTTPClient.Model
+{
+    public class Post
+    {
+        public string userId { get; set; }
+    }
+}
---
+++ View/PostView.xaml.cs
@@ -0,0 +1,9 @@
+namespace HTTPClient.View;
+
+public partial class PostView : ContentPage
+{
+	public PostView()
+	{
+		InitializeComponent();
+	}
+}
