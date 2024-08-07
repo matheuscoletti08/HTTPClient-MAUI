115efa51b7a9ee9f70811b999707bb473c820328
Coletti <matheus.coletti08@gmail.com>
2024-08-07 18:21:19 -0300

    feat: implement ViewModel in project

---
+++ ViewModel/PostViewModel.cs
@@ -0,0 +1,21 @@
+﻿using HTTPClient.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace HTTPClient.ViewModel
+{
+    public partial class PostViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        List<Post> posts;
+
+        public void getPosts()
+        {
+
+        }
+    }
+}
