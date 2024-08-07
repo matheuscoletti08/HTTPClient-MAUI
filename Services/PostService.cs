e0d7a6d1ac520adb36c45dc00f1cf46b19fcaf18
Coletti <matheus.coletti08@gmail.com>
2024-08-07 18:22:06 -0300

    feat: add Services in project

---
+++ Services/PostService.cs
@@ -0,0 +1,16 @@
+﻿using HTTPClient.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HTTPClient.Services
+{
+    internal class PostService
+    {
+        private HttpClient httpClient;
+        private Post post;
+        private List<Post> posts;
+    }
+}
